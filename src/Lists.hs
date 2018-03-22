4ea065adf0d5822b9d2b1b9207499f926b02c068
Samuel Butta <samuel@butta.cz>
2018-03-22 16:45:42 +0100

    Implement matrixMultiplication function

--- src/Lists.hs
+++ src/Lists.hs
@@ -1,5 +1,7 @@
 module Lists where
 
+import Data.List
+
 -- https://en.wikipedia.org/wiki/Pythagorean_triple
 -- List is ordered by x,y,z in ascending order, contains also primitive triples
 pythagoreanTriples :: [(Integer, Integer, Integer)]
@@ -13,10 +15,10 @@ eyeMatrix n
        | otherwise = [[ if k == x then 1 else 0 | x <- [1.. toInteger n], let k=y ]| y <- [1.. toInteger n]]
 
 
--- TODO: multiply matrices x and y
--- TODO: use list comprehension!
 -- https://en.wikipedia.org/wiki/Matrix_multiplication
 -- Note: sublists are rows
 -- if wrong sizes, raise error "Incorrect matrix sizes"
 matrixMultiplication :: Num a => [[a]] -> [[a]] -> [[a]]
-matrixMultiplication x y = undefined
+matrixMultiplication x y
+         | length (x !! 0) /= length y = error "Incorrect matrix sizes"
+         | otherwise = [[ sum (zipWith (*) a b) | b <- (transpose y) ] | a <- x ]
